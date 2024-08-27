9b69ae98a5a50deb3f11731647ddfceda1cc240d
AL3C-J <alecallan02@gmail.com>
2024-08-27 17:37:07 +1000

    updated_working(removed-extracode)

--- scripts/marker_tracking.py
+++ scripts/marker_tracking.py
@@ -3,10 +3,7 @@ import numpy as np
 import cv2
 import cv2.aruco as aruco
 import rospy
-from geometry_msgs.msg import PoseStamped
 from tf.transformations import quaternion_from_euler
-from geometry_msgs.msg import PoseStamped, TransformStamped  # Correct import for TransformStamped 
-import tf_conversions
 import tf2_ros
 import geometry_msgs.msg
 import math
@@ -22,16 +19,11 @@ dist_coeffs = np.array([0.2929, -1.1367, 0, 0, 0])
 marker_sizes = {
     5: 0.081  } 
 #-------------------------------------------#
-def normalize_angle(angle): # function used later to try and stabalise angles
-    return (angle + 180) % 360 - 180
-
 def rotation_matrix_to_euler_angles(R):
     """
     Converts a rotation matrix to Euler angles (in radians).
     Assumes the rotation matrix is for rotations about the x, y, and z axes in the order z-y-x (intrinsic rotations).
-
-    Returns:
-    (roll, pitch, yaw)
+    Returns:(roll, pitch, yaw)
     """
     sy = np.sqrt(R[0, 0] ** 2 + R[1, 0] ** 2)
 
@@ -57,10 +49,9 @@ def rover_pose_callback(msg):
     rover_position = (msg.pose.position.x, msg.pose.position.y, msg.pose.position.z)
 
 def main():
-    rospy.init_node("node_marker_tracking")
-    
+    rospy.init_node("node_marker_tracking")  
     # Load the video file
-    video_capture = cv2.VideoCapture('/home/localise_rover/catkin_ws/src/Localisation-System/videos/test_video_pass2.mov')
+    video_capture = cv2.VideoCapture('/home/localise_rover/catkin_ws/src/Localisation-System/videos/rotate_rover_test_3.MOV')
     
     # Create a dictionary of markers (retrieve information for marker recognition)
     dictionary = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
@@ -88,7 +79,6 @@ def main():
         y_base_link = 0
         z_base_link = 0
 
-
         # Calculate rvecs and tvecs for Marker 5
         if corners is not None and ids is not None:
             detected_ids = ids.flatten()
@@ -98,29 +88,16 @@ def main():
                 rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners[i], marker_size, camera_matrix, dist_coeffs)
                 frame_markers = cv2.drawFrameAxes(frame_markers, camera_matrix, dist_coeffs, rvecs[0], tvecs[0], 0.1)
                 x, y, z = tvecs[0][0]
-                 # Log the rover's center position (TOGGLE FOR DEBUGGING)
-                #rospy.loginfo(f"DEBUGGING MARKER POS -> x: {x}, y: {y}, z: {z}")
 
                 rotation_matrix, _ = cv2.Rodrigues(rvecs)
                 # Optional: Convert rotation matrix to Euler angles
                 euler_angles = rotation_matrix_to_euler_angles(rotation_matrix)
-                euler_angles_deg = np.degrees(euler_angles)
-                #x_deg, y_deg, z_deg = np.degrees([x_rvec, y_rvec, z_rvec])
-                #rospy.loginfo(f"DEBUGGING MARKER Angles -> r: x{x_deg}, p: {y_deg}, y: {z_deg}")
+                euler_angles_deg = np.degrees(euler_angles)               
 
                 print("Euler Angles (degrees):", euler_angles_deg)
 
                 x_rvec, y_rvec, z_rvec = euler_angles[0], euler_angles[1], euler_angles[2]
-                
-                
-                #x_deg = normalize_angle(x_deg)
-                #y_deg = normalize_angle(y_deg)
-                #z_deg = normalize_angle(z_deg)
-                #rospy.loginfo(f"DEBUGGING MARKER Angles -> r: {x_deg}, p: {y_deg}, y: {z_deg}")
-
-                
-                #------------------PUBLISH POSE INFORMATION (x, y, z, qx, qy, qz, qw)----------------------#
-                #qx, qy, qz, qw = quaternion_from_euler(float(x_rvec), float(y_rvec), float(z_rvec))
+                                      
                 q = quaternion_from_euler(float(x_rvec), float(y_rvec), float(z_rvec))
                 qx = q[0]
                 qy = q[1]
@@ -128,7 +105,6 @@ def main():
                 qw = q[3]
 
                 #-------------BROADCAST TRAN-------------#
-
                 br = tf2_ros.TransformBroadcaster()
                 t = geometry_msgs.msg.TransformStamped()
 
@@ -142,25 +118,18 @@ def main():
                 t.transform.rotation.y = qy
                 t.transform.rotation.z = qz
                 t.transform.rotation.w = qw
-
                 br.sendTransform(t)
                 
                 try:
-                    # Get the transform from 'map' to 'base_link'
-                    #transform = tf_buffer.lookup_transform('base_link', 'marker_frame', rospy.Time(0))
+                    # Get the transform from 'camera' to 'base_link'                  
                     transform = tf_buffer.lookup_transform('camera_frame', 'base_link', rospy.Time(0))
-
                     # Extract translation components (x, y, z)
                     x_base_link = transform.transform.translation.x
                     y_base_link = transform.transform.translation.y
                     z_base_link = transform.transform.translation.z
 
-                    # Optionally log the values for debugging
-                    #rospy.loginfo(f"Published Translation: x={x_base_link}, y={y_base_link}, z={z_base_link}")
-
                 except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
                     rospy.logwarn(f"Transform error: {e}")
-
 
                 #----------------------------------Display 6-States on Screen------------------------------#
             position_text = f"Marker Pos (x,y,z): {x:.2f}, {y:.2f}, {z:.2f} meters"
@@ -174,23 +143,13 @@ def main():
             cv2.putText(frame_markers, position_text, (20, 60), cv2.FONT_HERSHEY_SIMPLEX, font_scale, (0, 0, 255), font_thickness, cv2.LINE_AA)
             cv2.putText(frame_markers, rotation_text, (20, 100), cv2.FONT_HERSHEY_SIMPLEX, font_scale, (0, 0, 255), font_thickness, cv2.LINE_AA)
         
-            # -------------------- Draw Rover Center Dot -------------------- #
-            #if rover_position is not None:
-            # Convert rover_position to a 2D array with shape (1, 3)
-            point_3d = np.array([[x_base_link, y_base_link, z_base_link]], dtype=np.float64)             
-            # Print the shape of rover_position for debugging
-            #rospy.loginfo(f"rover_position 3D: {point_3d}")
-            
+            # -------------------- Draw Rover Center Dot -------------------- #           
+            point_3d = np.array([[x_base_link, y_base_link, z_base_link]], dtype=np.float64)                       
             # Project the 3D point to 2D
-            point_2d, _ = cv2.projectPoints(point_3d, np.zeros((3, 1)), np.zeros((3, 1)), camera_matrix, dist_coeffs)               
-            # Print the projected point [TOGGLE for DEBUGGING]
-            #rospy.loginfo(f"Projected 2D: {point_2d}")
-
+            point_2d, _ = cv2.projectPoints(point_3d, np.zeros((3, 1)), np.zeros((3, 1)), camera_matrix, dist_coeffs)                          
             # Draw the point on the image
             pixel_coordinates = tuple(int(c) for c in point_2d.ravel())
-            cv2.circle(frame_markers, pixel_coordinates, 10, (0, 255, 0), -1)
-            #else:
-                #rospy.logwarn("rover_position is None, skipping projection.")
+            cv2.circle(frame_markers, pixel_coordinates, 10, (0, 255, 0), -1)           
             # --------------------------------------------------------------- #
 
         cv2.imshow('Frame with Pose Estimation', frame_markers)
